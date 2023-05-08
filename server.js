947b66c4af12f1d3a4d5dc837a92b3d3aa2cac44
a42f9b2c34582681d2ef4f51ef75070144258469 <lykawangg@gmail.com>
2023-05-07 23:11:28 -0700

    POST and GET APIs to MongoDB database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel')
 const app = express();
+
+
+app.use(express.json())
 
 // routes
 
@@ -9,16 +13,47 @@ app.get('/', (req, res) => {
 })
 
 app.get('/blog', (req, res) => {
-    res.send('Hello, blog! My name is Pikachu')
+    res.send('Hello, blog!')
+})
+
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/products', async(req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message })
+    }
 })
 
 mongoose
-.connect('mongodb+srv://admin:0306@cluster0.ukc5yxi.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(() => {
-    console.log('connected to MongoDB')
-    app.listen(3000, ()=> {
-        console.log(`Node API app is running on port 3000 now`)
-    });
-}).catch((error) => {
-    console.log(error)
-})
+    .connect('mongodb+srv://admin:0306@cluster0.ukc5yxi.mongodb.net/Node-API?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('connected to MongoDB')
+        app.listen(3000, () => {
+            console.log(`Node API app is running on port 3000 now`)
+        });
+    }).catch((error) => {
+        console.log(error)
+    })
