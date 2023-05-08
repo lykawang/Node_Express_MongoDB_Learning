54ed5fd39c3cc631e56b4fe6f741171109c962b2
a42f9b2c34582681d2ef4f51ef75070144258469 <lykawangg@gmail.com>
2023-05-07 22:32:33 -0700

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // routes
@@ -7,6 +8,17 @@ app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
-app.listen(3000, ()=> {
-    console.log(`Node API app is running on port 3000 now`)
+app.get('/blog', (req, res) => {
+    res.send('Hello, blog! My name is Pikachu')
+})
+
+mongoose
+.connect('mongodb+srv://admin:0306@cluster0.ukc5yxi.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log(`Node API app is running on port 3000 now`)
+    });
+}).catch((error) => {
+    console.log(error)
 })
